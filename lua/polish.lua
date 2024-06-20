89a31e5b20a6a71a47d9805cdebb0faebeda3478
Roman Freiberg <me@romanfreiberg.com>
2024-06-20 14:44:18 +0200

    update to simple python ready version

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,18 +1,55 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+local dap = require('dap')
 
--- This will run last in the setup process and is a good place to configure
--- things like custom filetypes. This just pure lua so anything that doesn't
--- fit in the normal config locations above can go here
+local function load_project_specific_dap()
+  local dap_config_path = vim.fn.getcwd() .. '/.vim/dap.lua'
+  if vim.fn.filereadable(dap_config_path) == 1 then
+    local project_dap = dofile(dap_config_path)
+    if project_dap and project_dap.dap then
+      if project_dap.dap.adapters then
+        for adapter_name, adapter_config in pairs(project_dap.dap.adapters) do
+          dap.adapters[adapter_name] = adapter_config
+        end
+      end
+      if project_dap.dap.configurations then
+        for language, configs in pairs(project_dap.dap.configurations) do
+          dap.configurations[language] = configs
+        end
+      end
+    end
+  else
+    -- Fallback to default configuration if `.vim/dap.lua` does not exist
+    dap.adapters.python = {
+      type = "executable",
+      command = "/usr/bin/python3",
+      args = { "-m", "debugpy.adapter" },
+    }
+    dap.configurations.python = {
+      {
+        type = "python",
+        request = "launch",
+        name = "Launch file",
+        program = "${file}",
+        pythonPath = function()
+          local conda_prefix = os.getenv("CONDA_PREFIX")
+          if conda_prefix then
+            return conda_prefix .. "/bin/python"
+          else
+            local cwd = vim.fn.getcwd()
+            if vim.fn.executable(cwd .. "/venv/bin/python") == 1 then
+              return cwd .. "/venv/bin/python"
+            elseif vim.fn.executable(cwd .. "/.venv/bin/python") == 1 then
+              return cwd .. "/.venv/bin/python"
+            else
+              return "/usr/bin/python"
+            end
+          end
+        end,
+        cwd = function()
+          return vim.fn.getcwd()
+        end,
+      },
+    }
+  end
+end
 
--- Set up custom filetypes
-vim.filetype.add {
-  extension = {
-    foo = "fooscript",
-  },
-  filename = {
-    ["Foofile"] = "fooscript",
-  },
-  pattern = {
-    ["~/%.config/foo/.*"] = "fooscript",
-  },
-}
+load_project_specific_dap()
